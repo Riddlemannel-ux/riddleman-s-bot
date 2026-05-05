fc51a4af6229c8a841c8fd61c15bc74a38a68577
Riddlemannel-ux <omoghenebillions3@gmail.com>
2026-05-05 16:35:29 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,1 +1,31 @@
-u
+const fs = require("fs")
+
+module.exports = {
+  name: "menu",
+  run: async (sock, sender, args) => {
+
+    const files = fs.readdirSync("./plugins").filter(f => f.endsWith(".js"))
+
+    let list = ""
+
+    for (let file of files) {
+      const plugin = require(`./${file}`)
+      if (plugin.name) {
+        list += `│  ◈  .${plugin.name}\n`
+      }
+    }
+
+    const menu = `
+╭══〘 𝗥𝗜𝗗𝗗𝗟𝗘 𝗠𝗔𝗡 𝗕𝗢𝗧 〙═⊷
+│↠🤖 Bot Menu
+│↠📦 Total Cmds: ${files.length}
+├─────────────────
+${list}
+╰═════════════════⊷
+
+ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʀɪᴅᴅʟᴇ ᴍᴀɴ
+`
+
+    await sock.sendMessage(sender, { text: menu })
+  }
+}
